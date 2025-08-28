19cddd12e4b5bbe704127fa05d70b67bd4d92711
jeronimo <sajeronimo@slc.edu.gt>
2025-08-28 08:48:00 -0600

    hola

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
-// Se importa la dependencia dotenv para administrar las variables
-// de entorno del archivo .env
-require('dotenv').config();
-
-
-// importando el módulo express
-const express = require('express');
-const cors = require('cors');  
-
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
 const app = express();
 
+// Habilitar CORS antes de las rutas
+app.use(
+  cors({
+    origin: [
+        "*",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500/pages/dashboard.html"
 
-//CORS
-app.use(cors());
+    ],
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
+app.use(express.json());
 
-const getTablas = require('./routes/get/obtenerTablas');
-app.use(getTablas);
+// Rutas
+const getTablas = require("./routes/get/obtenerTablas");
+const getTareas = require("./routes/get/obtenerTareas");
 
+app.use("/api", getTablas);
+app.use("/api", getTareas);
 
-const getTareas = require('./routes/get/obtenerTareas');
-app.use(getTareas);
-    
-
-// CONFIGURACIÓN DEL SERVIDOR
-// process.env.PORT: Lee el puerto desde las variables de entorno (archivo .env)
-// || 3000: Si no existe, usa el puerto 3000 (operador "OR" de JavaScript)
+// Servidor
 const PORT = process.env.PORT || 3000;
-
-
-// Crea un servidor web y lo deja "escuchando" en el puerto 3000
-// Permite que tu backend reciba solicitudes (GET, POST, etc.)
-// desde clientes (navegadores, apps móviles, etc.).
-app.listen(PORT,()=>{
-    console.log(`Servidor: http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
